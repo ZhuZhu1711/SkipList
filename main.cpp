c75038a08ca99cecda732b32835ad914af587dff
xhu-zdx <212021085400054@stu.xhu.edu.cn>
2024-03-30 17:00:29 +0800

    insert

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 #include "skiplist.hpp"
 
 
 int main() {
+	srand(time(nullptr));
 	SkipList<int, std::string> skipList(6);
+	skipList.insert_element(1, "学");
+	skipList.insert_element(3, "算法");
+	skipList.insert_element(7, "认准");
+	skipList.insert_element(8, "微信公众号：代码随想录");
+	skipList.insert_element(9, "学习");
+	skipList.insert_element(19, "算法不迷路");
+	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！");
 
 	return 0;
 }
--- skiplist.hpp
+++ skiplist.hpp
@@ -7,7 +7,10 @@
 #include <fstream>
 #include <memory.h>
 #include <cstdio>
+#include <algorithm>
 
+
+std::mutex mtx;
 
 template<typename K, typename V>
 class Node {
@@ -16,9 +19,9 @@ public:
 	Node(K k, V v, int);
 	~Node();
 
-	//K get_key() const;
-	//V get_value() const;
-	//void set_value(V);
+	K get_key() const;
+	V get_value() const;
+	void set_value(V);
 
 	Node<K, V>** forward;			// 指向Node指针的指针
 	int node_level;
@@ -34,9 +37,9 @@ public:
 	SkipList(int);
 	~SkipList();
 
-//	int get_random_level();
-//	Node<K, V>* create_node(K, V, int);
-//	int insert_element();
+	int get_random_level();
+	Node<K, V>* create_node(K, V, int);
+	int insert_element(K, V);
 //	void display_list();
 //	bool search_element(K);
 //	void delete_element(K);
@@ -60,7 +63,8 @@ private:
 };
 
 
-
+// *********************************
+// 节点的类模板实现
 template<typename K, typename V>
 Node<K, V>::Node(const K k, const V v, int level) {
 	this->key = k;
@@ -76,13 +80,30 @@ Node<K, V>::~Node() {
 }
 
 template<typename K, typename V>
+V Node<K, V>::get_value() const{
+	return this->val;
+}
+
+template<typename K, typename V>
+K Node<K, V>::get_key() const {
+	return this->key;
+}
+
+template<typename K, typename V>
+void Node<K, V>::set_value(V val) {
+	this->val = val;
+}
+
+// **************************
+// SkipList的类模板实现
+template<typename K, typename V>
 SkipList<K, V>::SkipList(int maxLevel) {
 	this->_max_level = maxLevel;
 	this->_skip_list_level = 0;
 	this->_element_count = 0;
 
-	K k = 0;
-	V v;									// 局部变量必须初始化，std::string有默认初始化
+	K k;
+	V v;
 	this->_header = new Node<K, V>(k, v, _max_level);
 }
 
@@ -105,4 +126,71 @@ void SkipList<K, V>::clear(Node<K, V>* cur) {
 		clear(cur->forward[0]);
 	}
 	delete(cur);
+}
+
+template<typename K, typename V>
+Node<K, V>* SkipList<K, V>::create_node(const K key, const V val, int level) {
+	Node<K, V>* node = new Node<K, V>(key, val, level);
+	return node;
+}
+
+template<typename K, typename V>
+int SkipList<K, V>::insert_element(const K key, const V val) {
+	mtx.lock();
+	Node<K, V>* current = this->_header;
+	Node<K, V>* update[this->_max_level + 1];
+
+	memset(update, 0, sizeof(Node<K, V>*) * (this->_max_level + 1));
+
+	// 从跳表的最高层级开始
+	for (int i = this->_skip_list_level; i >= 0; i--) {	
+		// 在每个层级顺序搜索i应该插入的坐标
+		while (current->forward[i] != NULL && current->forward[i]->get_key() < key) {
+			current = current->forward[i];
+		}
+		update[i] = current;
+	}
+
+	current = current->forward[0];
+
+	// 键已存在
+	if (current != NULL && current->get_key() == key) {
+		std::cout << "key: " << key << ",exists" << std::endl;
+		mtx.unlock();
+		return 1;
+	}
+
+	// current是NULL，说明已经找到了最后的层级
+	// 如果current的key不是要插入的key，就插入当前key
+	if (current == NULL || current->get_key() != key) {
+		int random_level = get_random_level();
+		if (random_level > this->_skip_list_level) {
+			for (int i = this->_skip_list_level + 1; i < random_level + 1; i++) {
+				update[i] = _header;
+			}
+			this->_skip_list_level = random_level;
+		}
+		Node<K, V>* inserted_node = create_node(key, val, random_level);
+		for (int i = 0; i < random_level; i++) {
+			inserted_node->forward[i] = update[i]->forward[i];
+			update[i]->forward[i] = inserted_node;
+		}
+		std::cout << "Successfully inserted { " << key << " : " << " \"" << val << "\"" << " }" << std::endl;
+		this->_element_count++;
+	}
+	mtx.unlock();
+
+	return 0;
+}
+
+template<typename K, typename V>
+int SkipList<K, V>::get_random_level() {
+	int k = 1;
+	int r = rand();
+	while (r%2) {
+		k++;
+		r = rand();
+	}
+
+	return std::min(k, this->_max_level);
 }
